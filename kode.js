4b0e7eb70582091617c67be30eafe99918a74dbd
Pepep21 <146739403+Pepep21@users.noreply.github.com>
2023-10-02 20:47:18 +0700

    Create kode.js

---
+++ kode.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f052dc60e75380b5464ed3fa6d86a369',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/f052dc60e75380b5464ed3fa6d86a369/invoke.js"></scr' + 'ipt>')
